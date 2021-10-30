277cf7455a0ba8fba90eeb7f44e80f9e305651cf
Cahya Wirawan <Cahya.Wirawan@gmail.com>
2021-10-30 16:21:46 +0000

    add files for single models

--- run_finetuning_su.sh
+++ run_finetuning_su.sh
@@ -4,6 +4,6 @@ export WANDB_ENTITY="cahya"
 export WANDB_PROJECT="wav2vec2-multilang-asr"
 export WANDB_LOG_MODEL="true"
 
-python -m torch.distributed.launch --nproc_per_node=8 run_finetuning.py jv_su.json
+python -m torch.distributed.launch --nproc_per_node=8 run_finetuning.py su.json
 
 
